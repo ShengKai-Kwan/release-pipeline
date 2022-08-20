3ef3291a324e88ca0a9e772f08a10960402f0337
shengkai kwan <skkwan97@gmail.com>
2022-08-20 13:33:24 +0800

    initial

---
+++ build-tool.bat
@@ -0,0 +1,9 @@
+REM ==========================================
+REM === Build Tool For POS System
+REM ==========================================
+
+REM ==== Build Ms-Inventory Image - Start ====
+cd ./ms-inventory
+./docker_build.bat
+cd ..
+REM ==== Build Ms-Inventory Image - End ====
